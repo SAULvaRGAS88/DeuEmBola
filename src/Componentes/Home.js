024f7293facf914aa885e937ecfdc2837fa4e83b
Saul <saulvargascosta@hotmail.com>
2024-03-18 23:50:19 -0300

    Menu home

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { FundoPaginas } from './FundoPAginas';
 import './Estilos/HomeCss.css';
 import { Card } from '@mui/material';
+import MenuConponente from './MenuComponente';
+
 
 export const Home = () => {
+  const menuItems = [
+    { label: 'Home' },
+    { label: 'Quem Somos' },
+    { label: 'Galeria' },
+    { label: 'Conatto' },
+  ];
+
   return (
     <div>
       <FundoPaginas>
-        <Card className='card-home'>  
-        
-          
+        <Card className='card-home'>
+          <div>
+            <MenuConponente menuItems={menuItems} />
+          </div>
+
         </Card>
       </FundoPaginas>
 
